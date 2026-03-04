8e720527d6a724475cc06b4dc0add98b54c0f419
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-03-03 21:59:28 -0500

    persona java

---
+++ src/Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+    final String nombre;
+    private final int edad;
+
+    // CONSTRUCTOR DE LA PERSONA
+    public Persona(String _nombre, int _edad) {
+        this.nombre = _nombre;
+        this.edad = _edad;
+    }
+
+    // METODOS DE LA PERSONA
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre); // cout <<
+        System.out.println("Edad: " + edad); // cout <<
+    }
+}
