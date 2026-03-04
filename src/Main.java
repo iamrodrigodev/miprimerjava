d1c9458ad5486555fec92945ff5d12cefc3e78c2
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-03-03 22:29:09 -0500

    comentarios

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); // iniciar el cin >>
+        //Scanner sc = new Scanner(System.in); // iniciar el cin >>
 
         Persona p1 = new Persona("Rodrigo", 50);
-        Persona p2 = new Persona("Rodrigo", 50);
-        Persona p3 = new Persona("Rodrigo", 50);
+        //Persona p2 = new Persona("Rodrigo", 50);
+        //Persona p3 = new Persona("Rodrigo", 50);
 
 
         p1.obtenerInformacion();
